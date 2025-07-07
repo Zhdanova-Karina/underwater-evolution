68bc35782f3fd4ff79bcc218c0d5d3a0134e76cc
Karina <14150226c@gmail.com>
2025-07-07 18:43:28 +0700

    Update App.js
    травоядные могут маскироваться за кораллами

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import Predator from './Predator';
 import Omnivore from './Omnivore';
 import Plant from './Plant';
 import Pollution from './Pollution';
+import Coral from './Coral';
 import { checkBoundaries, zigzagEscape } from './utils';
-
 
 function UnderwaterEvolution() {
   const canvasRef = useRef(null);
@@ -14,6 +14,7 @@ function UnderwaterEvolution() {
   const [plants, setPlants] = useState([]);
   const [isRunning, setIsRunning] = useState(false);
   const [speedMultiplier, setSpeedMultiplier] = useState(1);
+  const [corals, setCorals] = useState([]);
   const [stats, setStats] = useState({
     herbivores: 0,
     predators: 0,
@@ -26,7 +27,8 @@ function UnderwaterEvolution() {
     herbivore: 1,
     predator: 1,
     omnivore: 1,
-    plant: 1
+    plant: 1,
+    coral: 1
   });
 
   const baseSpeed = 100;
@@ -62,14 +64,21 @@ function UnderwaterEvolution() {
       initialPlants.push(new Plant(i, Math.random() * 1300, Math.random() * 840));
     }
     
+    const initialCorals = [];
+    for (let i = 0; i < 10; i++) {
+      initialCorals.push(new Coral(i, Math.random() * 1300, Math.random() * 840));
+    }
+    
     setOrganisms(initialOrganisms);
     setPlants(initialPlants);
+    setCorals(initialCorals);
     setPollutions([]);
     setStats({
       herbivores: 5,
       predators: 5,
       omnivores: 5,
       plants: 20,
+      corals: 10,
       generation: 0
     });
     setIsRunning(false);
@@ -127,80 +136,123 @@ function UnderwaterEvolution() {
     
     const gameLoop = setInterval(() => {
       setOrganisms(prevOrganisms => {
+        // Обновляем состояние всех организмов
         const newOrganisms = prevOrganisms.map(org => {
           let newOrg;
           if (org.type === 'herbivore') {
             newOrg = new Herbivore(org.id, org.x, org.y);
+            Object.assign(newOrg, org);
           } else if (org.type === 'predator') {
             newOrg = new Predator(org.id, org.x, org.y);
+            Object.assign(newOrg, org);
           } else if (org.type === 'omnivore') {
             newOrg = new Omnivore(org.id, org.x, org.y);
+            Object.assign(newOrg, org);
           }
-          
-          if (newOrg) {
-            newOrg.energy = org.energy;
-            newOrg.eatenCount = org.eatenCount;
-            newOrg.isAdult = org.isAdult;
-            newOrg.reproductionCooldown = org.reproductionCooldown;
-            newOrg.escapeMode = org.escapeMode;
-            newOrg.escapeTimer = org.escapeTimer;
-            newOrg.escapeAngle = org.escapeAngle;
-            return newOrg;
-          }
-          return org;
+          return newOrg || org;
         });
         
         const newPlants = [...plants];
         const organismsToAdd = [];
         
+        // Сбрасываем маскировку всех кораллов
+        const updatedCorals = corals.map(coral => {
+          const newCoral = new Coral(coral.id, coral.x, coral.y);
+          newCoral.maskedHerbivoreId = null;
+          return newCoral;
+        });
+        
+        
         const herbivores = newOrganisms.filter(o => o.type === 'herbivore');
         const predators = newOrganisms.filter(o => o.type === 'predator');
         const omnivores = newOrganisms.filter(o => o.type === 'omnivore');
         
-        for (const organism of newOrganisms) {
-          if (organism.energy <= 0) continue;
+        // Обрабатываем движение и питание травоядных
+        herbivores.forEach(herbivore => {
+          if (herbivore.energy <= 0) return;
           
-          if (organism.type === 'herbivore') {
-            organism.move(newPlants, herbivores, predators, omnivores);
-            const eaten = organism.eat(newPlants);
-if (eaten?.type === 'plant') {
-    const plantIndex = newPlants.findIndex(p => p.id === eaten.id);
-    if (plantIndex !== -1) {
-        newPlants.splice(plantIndex, 1);
-    }
-}
-          } else if (organism.type === 'predator') {
-            organism.move(herbivores, omnivores, predators);
-            const eaten = organism.eat(herbivores, omnivores);
-            if (eaten) {
-              const preyArray = eaten.type === 'herbivore' ? herbivores : omnivores;
-              const preyId = preyArray[eaten.index].id;
-              const preyIndex = newOrganisms.findIndex(o => o.id === preyId);
-              if (preyIndex !== -1) {
-                newOrganisms[preyIndex].energy = 0;
+          // Проверяем маскировку
+          herbivore.isDisguised = false;
+          
+          // Ищем ближайший коралл для маскировки
+          let closestCoral = null;
+          let minDistance = Infinity;
+          
+          updatedCorals.forEach(coral => {
+            if (coral.maskedHerbivoreId) return; // Коралл уже занят
+            
+            const dx = herbivore.x - coral.x;
+            const dy = herbivore.y - coral.y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+            
+            if (dist < coral.size + herbivore.size && dist < minDistance) {
+              minDistance = dist;
+              closestCoral = coral;
+            }
+          });
+          
+          // Если нашли свободный коралл рядом - маскируемся
+          if (closestCoral) {
+            closestCoral.maskedHerbivoreId = herbivore.id;
+            herbivore.isDisguised = true;
+            herbivore.x = closestCoral.x;
+            herbivore.y = closestCoral.y;
+          } else {
+            // Если не маскируемся - двигаемся и едим
+            herbivore.move(newPlants, herbivores, predators, omnivores, updatedCorals);
+            const eaten = herbivore.eat(newPlants);
+            
+            if (eaten?.type === 'plant') {
+              const plantIndex = newPlants.findIndex(p => p.id === eaten.id);
+              if (plantIndex !== -1) {
+                newPlants.splice(plantIndex, 1);
               }
             }
-          } else if (organism.type === 'omnivore') {
-            organism.move(newPlants, herbivores, predators, omnivores);
-            const eaten = organism.eat(newPlants, herbivores, predators);
-            if (eaten) {
-              if (eaten.type === 'plant') {
-                newPlants.splice(eaten.index, 1);
-              } else {
-                const preyArray = eaten.type === 'herbivore' ? herbivores : 
-                                eaten.type === 'predator' ? predators : null;
-                if (preyArray) {
-                  const preyId = preyArray[eaten.index].id;
-                  const preyIndex = newOrganisms.findIndex(o => o.id === preyId);
-                  if (preyIndex !== -1) {
-                    newOrganisms[preyIndex].energy = 0;
-                  }
+          }
+        });
+        
+        // Обрабатываем хищников
+        predators.forEach(predator => {
+          if (predator.energy <= 0) return;
+          
+          predator.move(herbivores, omnivores, predators);
+          const eaten = predator.eat(herbivores, omnivores);
+          
+          if (eaten) {
+            const preyArray = eaten.type === 'herbivore' ? herbivores : omnivores;
+            const preyId = preyArray[eaten.index].id;
+            const preyIndex = newOrganisms.findIndex(o => o.id === preyId);
+            if (preyIndex !== -1) {
+              newOrganisms[preyIndex].energy = 0;
+            }
+          }
+        });
+        
+        // Обрабатываем всеядных
+        omnivores.forEach(omnivore => {
+          if (omnivore.energy <= 0) return;
+          
+          omnivore.move(newPlants, herbivores, predators, omnivores);
+          const eaten = omnivore.eat(newPlants, herbivores, predators);
+          
+          if (eaten) {
+            if (eaten.type === 'plant') {
+              newPlants.splice(eaten.index, 1);
+            } else {
+              const preyArray = eaten.type === 'herbivore' ? herbivores : 
+                              eaten.type === 'predator' ? predators : null;
+              if (preyArray) {
+                const preyId = preyArray[eaten.index].id;
+                const preyIndex = newOrganisms.findIndex(o => o.id === preyId);
+                if (preyIndex !== -1) {
+                  newOrganisms[preyIndex].energy = 0;
                 }
               }
             }
           }
-        }
+        });
         
+        // Размножение
         for (let i = 0; i < newOrganisms.length; i++) {
           const org1 = newOrganisms[i];
           if (org1.energy <= 0 || !org1.isAdult) continue;
@@ -222,29 +274,46 @@ if (eaten?.type === 'plant') {
           }
         }
         
-        const updatedPollutions = pollutions
-          .map(p => {
-            const newP = new Pollution(p.id, p.x, p.y);
-            Object.assign(newP, p);
-            newP.update();
-            return newP;
-          })
-          .filter(p => p.isActive);
+        // Обновляем загрязнения
+const updatedPollutions = pollutions
+  .map(p => {
+    const newP = new Pollution(p.id, p.x, p.y);
+    Object.assign(newP, p);
+    newP.update(); // Это теперь будет изменять размер и прозрачность
+    return newP;
+  })
+  .filter(p => p.isActive); // Удаляем неактивные
         
-        const updatedPlants = newPlants.filter(plant => {
-          return !updatedPollutions.some(pollution => 
-            pollution.isPointInside(plant.x, plant.y)
-          );
-        });
-        
-        const updatedOrganisms = [...newOrganisms, ...organismsToAdd]
-          .filter(organism => {
-            if (organism.energy <= 0) return false;
-            return !updatedPollutions.some(pollution => pollution.isPointInside(organism.x, organism.y));
-          });
+        // Удаляем организмы, растения и кораллы, попавшие в загрязнение
+const updatedPlants = newPlants.filter(plant => {
+  return !updatedPollutions.some(pollution => 
+    pollution.isPointInside(plant.x, plant.y)
+  );
+});
+
+const updatedCoralsPollution = corals.filter(coral => {
+  return !updatedPollutions.some(pollution => 
+    pollution.isPointInside(coral.x, coral.y)
+  );
+});
+
+const updatedOrganisms = [...newOrganisms, ...organismsToAdd]
+  .filter(organism => {
+    if (organism.energy <= 0) return false;
+    return !updatedPollutions.some(pollution => pollution.isPointInside(organism.x, organism.y));
+  });
+
+// Освобождаем травоядных, чьи кораллы погибли
+const survivingCoralIds = new Set(updatedCorals.map(c => c.id));
+newOrganisms.forEach(org => {
+  if (org.type === 'herbivore' && org.currentCoralId && !survivingCoralIds.has(org.currentCoralId)) {
+    org.resetDisguise();
+  }
+});
         
         setPlants(updatedPlants);
         setPollutions(updatedPollutions);
+        setCorals(updatedCoralsPollution);
         
         setStats(prev => ({
           ...prev,
@@ -252,6 +321,7 @@ if (eaten?.type === 'plant') {
           predators: updatedOrganisms.filter(o => o.type === 'predator').length,
           omnivores: updatedOrganisms.filter(o => o.type === 'omnivore').length,
           plants: updatedPlants.length,
+          corals: updatedCorals.length,
           generation: prev.generation + 1
         }));
         
@@ -260,7 +330,7 @@ if (eaten?.type === 'plant') {
     }, actualSpeed);
 
     return () => clearInterval(gameLoop);
-  }, [isRunning, speedMultiplier, plants, pollutions]);
+  }, [isRunning, speedMultiplier, plants, pollutions, corals]);
 
   const renderScene = () => {
     const canvas = canvasRef.current;
@@ -269,31 +339,35 @@ if (eaten?.type === 'plant') {
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, 1300, 840);
     
+    // Рисуем фон
     ctx.fillStyle = '#5c9eee';
     ctx.fillRect(0, 0, 1300, 840);
     
-    pollutions.forEach(pollution => {
-      if (!pollution.isActive) return;
-      
-      const gradient = ctx.createRadialGradient(
-        pollution.x, pollution.y, 0,
-        pollution.x, pollution.y, pollution.currentSize
-      );
-      gradient.addColorStop(0, 'rgba(0, 0, 0, 0.8)');
-      gradient.addColorStop(1, 'rgba(0, 0, 0, 0.1)');
-      
-      ctx.fillStyle = gradient;
+    // Рисуем кораллы
+    corals.forEach(coral => {
+      ctx.fillStyle = coral.color;
       ctx.beginPath();
-      ctx.arc(
-        pollution.x, 
-        pollution.y, 
-        pollution.currentSize, 
-        0, 
-        Math.PI * 2
-      );
+      ctx.arc(coral.x, coral.y, coral.size, 0, Math.PI * 2);
       ctx.fill();
+      
+      // Добавляем детали к кораллам
+      ctx.fillStyle = 'rgba(0,0,0,0.1)';
+      for (let i = 0; i < 10; i++) {
+        const angle = Math.random() * Math.PI * 2;
+        const dist = Math.random() * coral.size;
+        ctx.beginPath();
+        ctx.arc(
+          coral.x + Math.cos(angle) * dist,
+          coral.y + Math.sin(angle) * dist,
+          1 + Math.random() * 3,
+          0,
+          Math.PI * 2
+        );
+        ctx.fill();
+      }
     });
     
+    // Рисуем растения
     plants.forEach(plant => {
       ctx.fillStyle = plant.color;
       ctx.beginPath();
@@ -301,48 +375,100 @@ if (eaten?.type === 'plant') {
       ctx.fill();
     });
     
+    // Рисуем организмы
     organisms.forEach(organism => {
-      ctx.fillStyle = organism.color;
-      ctx.beginPath();
-      ctx.arc(organism.x, organism.y, organism.size, 0, Math.PI * 2);
-      ctx.fill();
+      // Если травоядное замаскировано - рисуем его как часть коралла
+      if (organism.type === 'herbivore' && organism.isDisguised) {
+        // Тело замаскированного организма
+    ctx.fillStyle = organism.disguiseColor || '#8a6d3b';
+    ctx.beginPath();
+    ctx.arc(organism.x, organism.y, organism.size, 0, Math.PI * 2);
+    ctx.fill();
+    
+    // Глаза замаскированного организма (добавлено)
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(
+      organism.x - organism.size * 0.3, 
+      organism.y - organism.size * 0.2, 
+      organism.size * 0.3, 
+      0, 
+      Math.PI * 2
+    );
+    ctx.arc(
+      organism.x + organism.size * 0.3, 
+      organism.y - organism.size * 0.2, 
+      organism.size * 0.3, 
+      0, 
+      Math.PI * 2
+    );
+    ctx.fill();
+    
+    // Зрачки замаскированного организма (добавлено)
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(
+      organism.x - organism.size * 0.3, 
+      organism.y - organism.size * 0.2, 
+      organism.size * 0.15, 
+      0, 
+      Math.PI * 2
+    );
+    ctx.arc(
+      organism.x + organism.size * 0.3, 
+      organism.y - organism.size * 0.2, 
+      organism.size * 0.15, 
+      0, 
+      Math.PI * 2
+    );
+    ctx.fill();
+      } else {
+        // Рисуем обычный организм
+        ctx.fillStyle = organism.color;
+        ctx.beginPath();
+        ctx.arc(organism.x, organism.y, organism.size, 0, Math.PI * 2);
+        ctx.fill();
+        
+        // Глаза
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(
+          organism.x - organism.size * 0.3, 
+          organism.y - organism.size * 0.2, 
+          organism.size * 0.3, 
+          0, 
+          Math.PI * 2
+        );
+        ctx.arc(
+          organism.x + organism.size * 0.3, 
+          organism.y - organism.size * 0.2, 
+          organism.size * 0.3, 
+          0, 
+          Math.PI * 2
+        );
+        ctx.fill();
+        
+        // Зрачки
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(
+          organism.x - organism.size * 0.3, 
+          organism.y - organism.size * 0.2, 
+          organism.size * 0.15, 
+          0, 
+          Math.PI * 2
+        );
+        ctx.arc(
+          organism.x + organism.size * 0.3, 
+          organism.y - organism.size * 0.2, 
+          organism.size * 0.15, 
+          0, 
+          Math.PI * 2
+        );
+        ctx.fill();
+      }
       
-      ctx.fillStyle = 'white';
-      ctx.beginPath();
-      ctx.arc(
-        organism.x - organism.size * 0.3, 
-        organism.y - organism.size * 0.2, 
-        organism.size * 0.3, 
-        0, 
-        Math.PI * 2
-      );
-      ctx.arc(
-        organism.x + organism.size * 0.3, 
-        organism.y - organism.size * 0.2, 
-        organism.size * 0.3, 
-        0, 
-        Math.PI * 2
-      );
-      ctx.fill();
-      
-      ctx.fillStyle = 'black';
-      ctx.beginPath();
-      ctx.arc(
-        organism.x - organism.size * 0.3, 
-        organism.y - organism.size * 0.2, 
-        organism.size * 0.15, 
-        0, 
-        Math.PI * 2
-      );
-      ctx.arc(
-        organism.x + organism.size * 0.3, 
-        organism.y - organism.size * 0.2, 
-        organism.size * 0.15, 
-        0, 
-        Math.PI * 2
-      );
-      ctx.fill();
-      
+      // Обводка для взрослых особей
       if (organism.isAdult) {
         ctx.strokeStyle = 'gold';
         ctx.lineWidth = 2;
@@ -351,11 +477,35 @@ if (eaten?.type === 'plant') {
         ctx.stroke();
       }
     });
+    
+    // Рисуем загрязнения
+pollutions.forEach(pollution => {
+  ctx.fillStyle = pollution.color;
+  ctx.beginPath();
+  ctx.arc(pollution.x, pollution.y, pollution.size, 0, Math.PI * 2);
+  ctx.fill();
+  
+  // Добавляем эффект "грязи"
+  ctx.fillStyle = 'rgba(50, 50, 50, 0.2)';
+  for (let i = 0; i < 10; i++) {
+    const angle = Math.random() * Math.PI * 2;
+    const dist = Math.random() * pollution.size;
+    ctx.beginPath();
+    ctx.arc(
+      pollution.x + Math.cos(angle) * dist,
+      pollution.y + Math.sin(angle) * dist,
+      2 + Math.random() * 5,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+  }
+});
   };
 
   useEffect(() => {
     renderScene();
-  }, [organisms, plants]);
+  }, [organisms, plants, corals, pollutions]);
 
   return (
     <div className="evolution-simulator">
@@ -369,6 +519,8 @@ if (eaten?.type === 'plant') {
           <p>Хищники: {stats.predators}</p>
           <p>Всеядные: {stats.omnivores}</p>
           <p>Растения: {stats.plants}</p>
+          <p>Кораллы: {corals.length}</p>
+          <p>Замаскированные: {organisms.filter(o => o.isDisguised).length}</p>
         </div>
 
         <canvas 
@@ -449,6 +601,31 @@ if (eaten?.type === 'plant') {
               />
               <button onClick={addPlant}>Растение</button>
             </div>
+            
+            <div className="add-control">
+              <input 
+                type="number" 
+                min="1" 
+                max="20"
+                value={addCounts.coral}
+                onChange={(e) => setAddCounts(prev => ({
+                  ...prev,
+                  coral: Math.max(1, parseInt(e.target.value) || 1)
+                }))}
+              />
+              <button onClick={() => {
+                const newCorals = [];
+                for (let i = 0; i < addCounts.coral; i++) {
+                  newCorals.push(new Coral(
+                    Date.now() + i,
+                    Math.random() * 1300,
+                    Math.random() * 840
+                  ));
+                }
+                setCorals(prev => [...prev, ...newCorals]);
+              }}>Коралл</button>
+            </div>
+            
             <div className="add-control">
               <button onClick={addPollution}>Загрязнение</button>
             </div>
