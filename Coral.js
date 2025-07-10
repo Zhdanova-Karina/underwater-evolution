c65ec824351e0828fe3120015ab4a54a29e5dbd7
Karina <14150226c@gmail.com>
2025-07-10 18:24:48 +0700

    Update Coral.js
    Итог

--- Coral.js
+++ Coral.js
@@ -3,24 +3,12 @@ export default class Coral {
     this.id = id;
     this.x = x;
     this.y = y;
-    this.size = 8 + Math.random() * 7; // Увеличил размер
+    this.size = 8 + Math.random() * 7; 
     this.color = `hsl(${Math.random() * 30 + 350}, 70%, 60%)`;
     this.type = 'coral';
     this.maskedHerbivoreId = null; // ID травоядного, которое маскируется
-    this.health = 100;
   }
 
-  update() {
-    // Медленный рост
-    if (this.size < 50 && Math.random() < 0.01) {
-      this.size += 0.1;
-    }
-    
-    // Освобождаем маскировку, если травоядное исчезло
-    if (this.maskedHerbivoreId && Math.random() < 0.05) {
-      this.maskedHerbivoreId = null;
-    }
-  }
   // Проверка, может ли коралл принять травоядное
   canAcceptHerbivore(herbivore) {
     const dx = herbivore.x - this.x;
